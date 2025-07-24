fa633888bace79dec60fa6dc2613e791f0299514
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-07-24 13:54:45 -0700

    link to the source files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,51 +9,51 @@ import ThumbsUpDownButtons from "src/app/examples/thumbsUpDownButtons";
 export default function Home() {
   return (
     <div className="m-auto max-w-screen-lg">
-      <h1 className="text-2xl mt-4">Feedback Test Area</h1>
+      <h1 className="text-2xl mt-4">Feedback Test Area (<a href="https://github.com/ryan953/nextjs-test-feedback">source</a>)</h1>
       <p>
         This is an example Next.JS website that implements the Sentry Feedback SDK.
-        It shows how to trigger Feedbakc in various ways.
+        It shows how to trigger Feedback in various ways.
       </p>
       <ul className="raw flex flex-col gap-2">
         <li>
           <fieldset className="border-1 border-gray-300 rounded-md p-2">
-            <legend>Feedback Button</legend>
+            <legend>Feedback Button (<a href="https://github.com/ryan953/nextjs-test-feedback/blob/main/src/app/examples/feedbackButton.tsx">source</a>)</legend>
             <FeedbackButton />
           </fieldset>
         </li>
         <li>
           <fieldset className="border-1 border-gray-300 rounded-md p-2">
-            <legend>Toggle Feedback Button</legend>
+            <legend>Toggle Feedback Button (<a href="https://github.com/ryan953/nextjs-test-feedback/blob/main/src/app/examples/toggleFeedbackButton.tsx">source</a>)</legend>
             <ToggleFeedbackButton />
           </fieldset>
         </li>
         <li>
           <fieldset className="border-1 border-gray-300 rounded-md p-2">
-            <legend>Attach To Feedback Button</legend>
+            <legend>Attach To Feedback Button (<a href="https://github.com/ryan953/nextjs-test-feedback/blob/main/src/app/examples/attachToFeedbackButton.tsx">source</a>)</legend>
             <AttachToFeedbackButton />
           </fieldset>
         </li>
         <li>
           <fieldset className="border-1 border-gray-300 rounded-md p-2">
-            <legend>Create Feedback From Button</legend>
+            <legend>Create Feedback From Button (<a href="https://github.com/ryan953/nextjs-test-feedback/blob/main/src/app/examples/createFeedbackFormButton.tsx">source</a>)</legend>
             <CreateFeedbackFromButton />
           </fieldset>
         </li>
         <li>
           <fieldset className="border-1 border-gray-300 rounded-md p-2">
-            <legend>My Feedback Form</legend>
+            <legend>My Feedback Form (<a href="https://github.com/ryan953/nextjs-test-feedback/blob/main/src/app/examples/myFeedbackForm.tsx">source</a>)</legend>
             <MyFeedbackForm />
           </fieldset>
         </li>
         <li>
           <fieldset className="border-1 border-gray-300 rounded-md p-2">
-            <legend>Crash Report Button</legend>
+            <legend>Crash Report Button (<a href="https://github.com/ryan953/nextjs-test-feedback/blob/main/src/app/examples/crashReportButton.tsx">source</a>)</legend>
             <CrashReportButton />
           </fieldset>
         </li>
         <li>
           <fieldset className="border-1 border-gray-300 rounded-md p-2">
-            <legend>Thumbs Up/Down Buttons</legend>
+            <legend>Thumbs Up/Down Buttons (<a href="https://github.com/ryan953/nextjs-test-feedback/blob/main/src/app/examples/thumbsUpDownButtons.tsx">source</a>)</legend>
             <ThumbsUpDownButtons /> 
           </fieldset>
         </li>
